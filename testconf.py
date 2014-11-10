b7a0f27bc47c53c74effd304f27f11fa86d48946
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-11-10 16:46:25 +0100

    Config: Added corrected jets

--- testconf.py
+++ testconf.py
@@ -1,10 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
-
-# initialize MessageLogger and output report
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+# defaults and global tag
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'START53_V27::All'
+
 
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
     'file:/storage/8/dhaitz/testfiles/rundepMC.root'
@@ -13,11 +15,47 @@ process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
     #'file:/storage/8/dhaitz/testfiles/DYToEE_M-20_CT10_TuneZ2star_8TeV-powheg-pythia6--Summer12_DR53X-PU_S10_START53_V7A-v1--AODSIM.root'
     #'file:/storage/8/dhaitz/testfiles/TTJets_FullLeptMGDecays_8TeV-madgraph__Summer12_DR53X-PU_RD1_START53_V7N-v1__AODSIM.root'
     #'root://xrootd.unl.edu//store/mc/Summer12_DR53X/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/AODSIM/PU_RD1_START53_V7N-v1/20000/024197EF-59CF-E211-9DD3-001E67397CB0.root'
+    #'file:/storage/8/dhaitz/testfiles/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6__Summer12_DR53X-PU_RD1_START53_V7N-v1__AODSIM.root'
 ))
 
-process.demo = cms.EDAnalyzer('DemoAnalyzer')
+
+
+# corrected jets
+process.load('JetMETCorrections.Configuration.JetCorrectionProducers_cff')
+process.load('JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff')
+
+process.ak5PFL1L2L3 = cms.ESProducer(
+    'JetCorrectionESChain',
+    correctors = cms.vstring('ak5PFL1Fastjet','ak5PFL2Relative','ak5PFL3Absolute')
+)
+process.ak5PFJetsL1 = cms.EDProducer(
+    'PFJetCorrectionProducer',
+    src        = cms.InputTag('ak5PFJets'),
+    correctors = cms.vstring('ak5PFL1Fastjet')
+)
+
+process.jets = cms.Sequence(process.ak5PFJetsL1L2L3 * process.ak5PFJetsL1)
+
+
+
+# analyzer and output
+process.l1l2l3 = cms.EDAnalyzer('DemoAnalyzer',
+    jetCollection = cms.InputTag("ak5PFJetsL1L2L3")
+)
+process.l1 = cms.EDAnalyzer('DemoAnalyzer',
+    jetCollection = cms.InputTag("ak5PFJetsL1")
+)
+process.raw = cms.EDAnalyzer('DemoAnalyzer',
+    jetCollection = cms.InputTag("ak5PFJets")
+)
 
 process.TFileService = cms.Service("TFileService",
-        fileName = cms.string('recogen.root'))
+        fileName = cms.string('recogen.root')
+)
 
-process.p = cms.Path(process.demo)
+process.p = cms.Path(
+    process.jets *
+    process.l1 *
+    process.l1l2l3 *
+    process.raw
+)
